2707024a7a2797ed171edcd7e527c4449b5eb13c
yottapus <francesco.cerri2@studio.unibo.it>
2021-04-01 13:30:59 +0200

    comfy camera/topic selection; added compressed and depth data compatibility

---
+++ scripts/c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+camSel={'one': ( 1, 111),
+	'two': (2, 222)}
+
+print(camSel['one'][1])
--- scripts/roscam1.py
+++ scripts/roscam1.py
@@ -4,41 +4,85 @@ import rospy
 #from std_msgs.msg import String
 #from PIL import Image
 #import matplotlib.pyplot as plt
-#import numpy as np
+import numpy as np
 from sensor_msgs.msg import Image as sensImg
+from sensor_msgs.msg import CompressedImage as CsensImg
+#from sensor_msgs.msg import PointCloud2 as sensPCld
+
 from cv_bridge import CvBridge
-#import time
 bridge=CvBridge()
  
-def callback(raw_img):
-    #heigth,width=data.height, data.width
-    #dim=(width,heigth)
-    #print(dim)
-    #raw_img=data
+def callbackRaw(raw_img):
+    #imgDim=(width,heigth)=(data.height, data.width)
     cv_image=bridge.imgmsg_to_cv2(raw_img, desired_encoding='passthrough')
-    cv.waitKey(30)
-    cv.imshow("image", cv_image)
-    
-    
-def listener():
+    #cv.waitKey(15)#???
+    cv.imshow("raw image", cv_image)
+    cv.waitKey(15)    
  
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+def callbackCompr(cmpr_img):#(1)
+    imgarr = np.fromstring(cmpr_img.data, np.uint8)
+    # could possibly try also direct array recast, but already working
+    cv_image=cv.imdecode(imgarr,cv.IMREAD_UNCHANGED)
+    cv.imshow("compressed image", cv_image)
+    cv.waitKey(15)
+
+#def callbackComprDep(cmpr_img):TODO#(1)
+#    imgarr = np.fromstring(cmpr_img.data, np.uint8)
+#    cv_image=cv.imdecode(imgarr,cv.IMREAD_UNCHANGED)
+#    cv.imshow("image", cv_image)
+    cv.waitKey(15)
+    
+#def callbackPCld(pcld_img):#(3)
+##    no pcl_ros method in python, which worked smootlhy
+##    alternative is python_pcl
+##    cv_image=
+#    cv.imshow("point cloud image", cv_image)
+#    cv.waitKey(15)
+
+def listener(myCam,(myTop,myType,myCallk)):
     rospy.init_node('camera_listener', anonymous=True)
- 
-    rospy.Subscriber("/camera_image_fix/color/image_raw",sensImg, callback,queue_size = 1)
-    # spin() simply keeps python from exiting until this node is stopped
+    rospy.Subscriber(myCam+myTop,myType,myCallk,queue_size = 1)
+
     try:
         rospy.spin()
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:#what about adding waitKey() here?
         print('Closing')
-    cv.destroyAllWindows() 
+    cv.destroyAllWindows()
+    
+
+camDict={'moving':"/camera_image",
+            'fixed':"/camera_image_fix"}
+
+topicDict={'raw compressed':("/color/image_raw/compressed",
+                             CsensImg,
+                             callbackCompr),
+#                'raw depth':(imageTopic="/color/image_raw/compressedDepth",
+#                             imageType=CsensImg,
+#                             imageCallback=callbackComprDep),
+#                'point cloud':(imageTopic="/depth/color/points",
+#                               imageType=sensPCld,
+#                               imageCallback=callbackPCld),
+            'raw depth': ("/depth/image_rect_raw",
+                    sensImg,
+                    callbackRaw),
+            'raw':("/color/image_raw",
+                    sensImg,
+                    callbackRaw)    
+            }   
+
 if __name__ == '__main__':
-    print('starting')
-    listener()
+    myCamera=camDict['moving']
+    myTopicFull=topicDict['raw']
+    print('connecting to:'+myCamera+myTopicFull[0]+'...')
+    listener(myCamera,myTopicFull)
  
 
-
+#bibliography
+#     (1)
+#compressed images
+#cast in np array and cv2.imdecode
+#http://wiki.ros.org/rospy_tutorials/Tutorials/WritingImagePublisherSubscriber
+#    (2)
+#https://answers.ros.org/question/249775/display-compresseddepth-image-python-cv2/
+#   (3)
+#https://strawlab.github.io/python-pcl/
