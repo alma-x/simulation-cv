f561c0f8398bcbddf4f57371d6a74940359f297e
francesco <74739268+gnoccoalpesto@users.noreply.github.com>
2021-08-11 11:38:18 +0200

    Update arucoPanel.py

--- scripts/arucoPanel.py
+++ scripts/arucoPanel.py
@@ -55,7 +55,7 @@ def loadCameraParam(myCam):
     global cameraFocLen
     
     print('loading camera parameters...')
-    cameraInfoMsg=rospy.wait_for_message(myCam+'/color/camera_info',CameraInfo)
+    cameraInfoMsg=rospy.wait_for_message(myCam+'/camera_info',CameraInfo)
     cameraMatr=np.reshape(cameraInfoMsg.K,[3,3])
     cameraDistCoefs=cameraInfoMsg.D
     cameraFocLen=np.mean([np.ravel(cameraMatr[0])[0],np.ravel(cameraMatr[1])[1]])
